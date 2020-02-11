b8f286a4fe9107f9684ffbee4ef73ca66869c081
agile.zhou <kklldog@live.cn>
2020-02-11 17:03:19 +0800

    add adminConsole config verb

--- AgileConfig.Server.Apisite/Controllers/HomeController.cs
+++ AgileConfig.Server.Apisite/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AgileConfig.Server.Common;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AgileConfig.Server.Apisite.Controllers
@@ -10,11 +11,21 @@ namespace AgileConfig.Server.Apisite.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            if ("true".Equals(Configuration.Config["adminConsole"], StringComparison.CurrentCultureIgnoreCase))
+            {
+                return View();
+            }
+
+            return Content($"AgileConfig node running now , {DateTime.Now.ToLongTimeString()}");
         }
         public IActionResult GetView(string viewName)
         {
-            return View(viewName);
+            if ("true".Equals(Configuration.Config["adminConsole"], StringComparison.CurrentCultureIgnoreCase))
+            {
+                return View(viewName);
+            }
+
+            return Content("");
         }
     }
 }
