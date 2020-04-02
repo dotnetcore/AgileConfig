0950e756e7235acdfd3b922920a1c82de21713c9
kklldog <kklldog@qq.com>
2020-04-03 00:03:43 +0800

    版本回滚功能完成

--- AgileConfig.Server.Apisite/wwwroot/js/modifyLog.js
+++ AgileConfig.Server.Apisite/wwwroot/js/modifyLog.js
@@ -1,4 +1,4 @@
-﻿app.controller('modifyLogsCtrl', function ($scope, $http, $state, $stateParams) {
+﻿app.controller('modifyLogsCtrl', function ($scope, $http, $state, $stateParams, $filter) {
     let id = $stateParams.config_id;
     $scope.logs = [];
 
@@ -15,4 +15,33 @@
                 console.log(err);
                 alert(err.statusText);
             });
+    $scope.rollback = async function (log) {
+        let dateFilter = $filter('date');
+        let modifyTime = dateFilter(log.modifyTime, 'yyyy-MM-dd HH:mm:ss');
+        let cr = confirm(`是否确定回滚至此版本【${modifyTime}】？`);
+        if (!cr) {
+            return;
+        }
+        let configResult = await $http.get('/config/get?id=' + log.configId + '&_=' + new Date().getTime());
+        if (configResult.data.success) {
+            let config = configResult.data.data;
+            config.key = log.key;
+            config.group = log.group;
+            config.value = log.value;
+            $http.post('/config/edit', config)
+                .then(r => {
+                    if (r.data.success) {
+                        alert('回滚成功。');
+                        $state.go('config.list');
+                    } else {
+                        alert(r.data.message);
+                    }
+                }, err => {
+                    console.log(err);
+                    alert(err.statusText);
+                });
+        } else {
+            alert(configResult.data.message);
+        }
+    };
 });
