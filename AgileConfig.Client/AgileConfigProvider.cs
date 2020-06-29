c0ee764580688912e56b7ae459645f50daceff24
kklldog <kklldog@qq.com>
2020-06-29 23:13:57 +0800

    修复一行错误的注释

--- AgileConfig.Client/AgileConfigProvider.cs
+++ AgileConfig.Client/AgileConfigProvider.cs
@@ -14,8 +14,7 @@ namespace Agile.Config.Client
         }
 
         /// <summary>
-        /// load方法会通过http从服务端拉取所有的配置，需要注意的是load方法在加载所有配置后会启动一个websocket客户端跟服务端保持长连接，当websocket
-        /// 连接建立成功会调用一次load方法，所以系统刚启动的时候通常会出现两次http请求。
+        /// load方法调用ConfigClient的Connect方法,Connect方法会在连接成功后拉取所有的配置。
         /// </summary>
         public override void Load()
         {
