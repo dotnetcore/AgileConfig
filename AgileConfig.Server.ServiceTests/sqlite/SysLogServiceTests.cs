952b1ab000c55718ace4ed7eadb647c5fada0476
kklldog <kklldog@qq.com>
2020-09-23 11:39:18 +0800

    完善单元测试

---
+++ AgileConfig.Server.ServiceTests/sqlite/SysLogServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AgileConfig.Server.Service;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgileConfig.Server.Service.Tests
+{
+    [TestClass()]
+    public class SysLogServiceTests
+    {
+        [TestMethod()]
+        public void AddSysLogAsyncTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
