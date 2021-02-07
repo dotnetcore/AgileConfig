256d327b5b7525dead58cbd3848d010ff1dec6dc
kklldog <kklldog@qq.com>
2021-02-08 00:55:40 +0800

    react-ui-ant-pro 1st checkin

---
+++ AgileConfig.Server.UI/react-ui-antd/src/layouts/UserLayout.tsx
@@ -0,0 +1,69 @@
+import type { MenuDataItem } from '@ant-design/pro-layout';
+import { DefaultFooter, getMenuData, getPageTitle } from '@ant-design/pro-layout';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import type { ConnectProps } from 'umi';
+import { Link, SelectLang, useIntl, connect, FormattedMessage } from 'umi';
+import React from 'react';
+import type { ConnectState } from '@/models/connect';
+import logo from '../assets/logo.svg';
+import styles from './UserLayout.less';
+
+export type UserLayoutProps = {
+  breadcrumbNameMap: Record<string, MenuDataItem>;
+} & Partial<ConnectProps>;
+
+const UserLayout: React.FC<UserLayoutProps> = (props) => {
+  const {
+    route = {
+      routes: [],
+    },
+  } = props;
+  const { routes = [] } = route;
+  const {
+    children,
+    location = {
+      pathname: '',
+    },
+  } = props;
+  const { formatMessage } = useIntl();
+  const { breadcrumb } = getMenuData(routes);
+  const title = getPageTitle({
+    pathname: location.pathname,
+    formatMessage,
+    breadcrumb,
+    ...props,
+  });
+  return (
+    <HelmetProvider>
+      <Helmet>
+        <title>{title}</title>
+        <meta name="description" content={title} />
+      </Helmet>
+
+      <div className={styles.container}>
+        <div className={styles.lang}>
+          <SelectLang />
+        </div>
+        <div className={styles.content}>
+          <div className={styles.top}>
+            <div className={styles.header}>
+              <Link to="/">
+                <span className={styles.title}>AgileConfig</span>
+              </Link>
+            </div>
+            <div className={styles.desc}>
+              <FormattedMessage
+                id="pages.layouts.userLayout.title"
+                defaultMessage="AgileConfig"
+              />
+            </div>
+          </div>
+          {children}
+        </div>
+        <DefaultFooter />
+      </div>
+    </HelmetProvider>
+  );
+};
+
+export default connect(({ settings }: ConnectState) => ({ ...settings }))(UserLayout);
