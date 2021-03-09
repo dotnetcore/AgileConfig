dc15fa3449fc0a769019d4426d73e91c7adc7afd
kklldog <kklldog@qq.com>
2021-03-09 15:35:13 +0800

    格式化代码

--- AgileConfig.Server.UI/react-ui-antd/src/pages/Home/index.tsx
+++ AgileConfig.Server.UI/react-ui-antd/src/pages/Home/index.tsx
@@ -5,13 +5,13 @@ import styles from './index.less';
 import { queryAppcount, queryConfigcount, queryNodecount, queryServerNodeStatus } from './service';
 
 export type itemInfoProps = {
-  type:string,
+  type: string,
   icon: JSX.Element,
   count: number
 }
 
 const ItemInfo: React.FC<itemInfoProps> = (props) => {
-  const itemColor=()=>{
+  const itemColor = () => {
     if (props.type == 'node') {
       return '#1890ff';
     }
@@ -27,7 +27,7 @@ const ItemInfo: React.FC<itemInfoProps> = (props) => {
 
     return '';
   }
-  const itemName=()=>{
+  const itemName = () => {
     if (props.type == 'node') {
       return '节点';
     }
@@ -40,16 +40,16 @@ const ItemInfo: React.FC<itemInfoProps> = (props) => {
     if (props.type == 'client') {
       return '客户端';
     }
-  
+
     return '';
   }
   return (
-    <div  className={styles.item} style={{backgroundColor:itemColor()}}>
+    <div className={styles.item} style={{ backgroundColor: itemColor() }}>
       <div>
         <div className={styles.count}>{props.count}</div>
         <div className={styles.name}>
           {
-           itemName()
+            itemName()
           }
         </div>
       </div>
@@ -63,43 +63,43 @@ const ItemInfo: React.FC<itemInfoProps> = (props) => {
   );
 }
 const Summary: React.FC = () => {
-  const [appCount,setAppCount] = useState<number>(0);
-  const [configCount,setConfigCount] = useState<number>(0);
-  const [nodeCount,setNodecount] = useState<number>(0);
-  const [clientCount,setClientCount] = useState<number>(0);
+  const [appCount, setAppCount] = useState<number>(0);
+  const [configCount, setConfigCount] = useState<number>(0);
+  const [nodeCount, setNodecount] = useState<number>(0);
+  const [clientCount, setClientCount] = useState<number>(0);
 
-  useEffect(()=>{
-    queryNodecount().then(x=> setNodecount(x) );
-  },[]);
-  useEffect(()=>{
-    queryConfigcount().then(x=> setConfigCount(x) );
-  },[]);
-  useEffect(()=>{
-    queryAppcount().then(x=> setAppCount(x) );
-  },[]);
-  useEffect(()=>{
-    queryServerNodeStatus().then((x:{server_status:{clientCount:number}}[])=> {
-      let count:number = 0;
+  useEffect(() => {
+    queryNodecount().then(x => setNodecount(x));
+  }, []);
+  useEffect(() => {
+    queryConfigcount().then(x => setConfigCount(x));
+  }, []);
+  useEffect(() => {
+    queryAppcount().then(x => setAppCount(x));
+  }, []);
+  useEffect(() => {
+    queryServerNodeStatus().then((x: { server_status: { clientCount: number } }[]) => {
+      let count: number = 0;
       x.forEach(item => {
         count = count + item.server_status.clientCount;
       });
       setClientCount(count);
-    }  );
-  },[]);
+    });
+  }, []);
   return (
     <div className={styles.summary}>
-       <ItemInfo count={nodeCount} type="node" icon={<DatabaseOutlined ></DatabaseOutlined>}></ItemInfo>
-       <ItemInfo count={appCount} type="app" icon={<AppstoreOutlined ></AppstoreOutlined>}></ItemInfo>
-       <ItemInfo count={configCount} type="config" icon={<TableOutlined ></TableOutlined>}></ItemInfo>
-       <ItemInfo count={clientCount} type="client" icon={<ShrinkOutlined ></ShrinkOutlined>}></ItemInfo>
-       
+      <ItemInfo count={nodeCount} type="node" icon={<DatabaseOutlined ></DatabaseOutlined>}></ItemInfo>
+      <ItemInfo count={appCount} type="app" icon={<AppstoreOutlined ></AppstoreOutlined>}></ItemInfo>
+      <ItemInfo count={configCount} type="config" icon={<TableOutlined ></TableOutlined>}></ItemInfo>
+      <ItemInfo count={clientCount} type="client" icon={<ShrinkOutlined ></ShrinkOutlined>}></ItemInfo>
+
     </div>
   );
 }
-const home:React.FC = () =>  {
+const home: React.FC = () => {
   return (
     <PageContainer pageHeaderRender={false}>
-     <Summary></Summary>  
+      <Summary></Summary>
     </PageContainer>
   );
 }
