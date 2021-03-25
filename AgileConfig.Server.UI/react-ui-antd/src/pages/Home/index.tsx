3c376bc883a25ff312e98704db96cb812efdbcf2
kklldog <kklldog@qq.com>
2021-03-25 17:47:49 +0800

    首页新增一个统计bar图

--- AgileConfig.Server.UI/react-ui-antd/src/pages/Home/index.tsx
+++ AgileConfig.Server.UI/react-ui-antd/src/pages/Home/index.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react';
 import styles from './index.less';
 import { queryAppcount, queryConfigcount, queryNodecount, queryServerNodeStatus } from './service';
 import { history } from 'umi';
+import * as echarts from 'echarts';
 
 export type itemInfoProps = {
   type: string,
@@ -69,11 +70,16 @@ const ItemInfo: React.FC<itemInfoProps> = (props) => {
 
   );
 }
-const Summary: React.FC = () => {
+
+export type summaryProps = {
+  clientCount?: number,
+  nodeStatiInfo?: any
+}
+
+const Summary: React.FC<summaryProps> = (props) => {
   const [appCount, setAppCount] = useState<number>(0);
   const [configCount, setConfigCount] = useState<number>(0);
   const [nodeCount, setNodecount] = useState<number>(0);
-  const [clientCount, setClientCount] = useState<number>(0);
 
   useEffect(() => {
     queryNodecount().then(x => setNodecount(x));
@@ -84,15 +90,83 @@ const Summary: React.FC = () => {
   useEffect(() => {
     queryAppcount().then(x => setAppCount(x));
   }, []);
+
+  return (
+    <div className={styles.panel}>
+      <div className={styles.summary}>
+      <ItemInfo count={nodeCount} type="node" link="/node" icon={<DatabaseOutlined ></DatabaseOutlined>}></ItemInfo>
+      <ItemInfo count={appCount} type="app" link="/app" icon={<AppstoreOutlined ></AppstoreOutlined>}></ItemInfo>
+      <ItemInfo count={configCount} type="config" link="/app" icon={<TableOutlined ></TableOutlined>}></ItemInfo>
+      <ItemInfo count={props.clientCount??0} type="client" link="/client" icon={<ShrinkOutlined ></ShrinkOutlined>}></ItemInfo>
+      </div>
+    </div>
+  );
+}
+const NodeStatiCharts: React.FC<summaryProps> = (props)=> {
+  useEffect(() => {
+    const el = document.getElementById('nodeClientsChart');
+      if (el) {
+        const myChart = echarts.init(el);
+        const option = {
+          legend: {
+            data: ['客户端数量']
+          },
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+          },
+          title: {
+          },
+          xAxis: {
+              type: 'category',
+              data: props.nodeStatiInfo?.chartCategorys??[]
+          },
+          yAxis: {
+              type: 'value'
+          },
+          series: [{
+              name:'客户端数量',
+              data: props.nodeStatiInfo?.chartValues??[],
+              type: 'bar'
+          }]
+        };
+        myChart.setOption(option);
+      }
+  }, [props.nodeStatiInfo]);
+  return(
+    <div className={styles.panel}>
+      <div id="nodeClientsChart" className={styles.node_clients} >
+      </div>
+    </div>
+
+  );
+}
+const home: React.FC = () => {
+  const [serverNodeStatus, setServerNodeStatus] = useState<summaryProps>({
+    clientCount:0,
+    nodeStatiInfo: null
+  });
   const getServerNodeClientInfos = ()=>{
-    queryServerNodeStatus().then((x: { server_status: { clientCount: number } }[]) => {
+    queryServerNodeStatus().then((x: {  n:{ address: string},server_status: { clientCount: number } }[]) => {
       let count: number = 0;
+      const chartCategorys:string[] = [];
+      const chartValues:Number[]=[];
       x?.forEach(item => {
         if (item.server_status) {
           count = count + item.server_status.clientCount;
         }
+        chartCategorys.push(item.n.address);
+        chartValues.push(item.server_status.clientCount);
       });
-      setClientCount(count);
+      setServerNodeStatus({
+        clientCount: count,
+        nodeStatiInfo: {
+          chartCategorys: chartCategorys,
+          chartValues: chartValues
+        }
+      });
     });
   }
   useEffect(() => {
@@ -105,18 +179,11 @@ const Summary: React.FC = () => {
     return ()=> clearInterval(id);
   }, []);
   return (
-    <div className={styles.summary}>
-      <ItemInfo count={nodeCount} type="node" link="/node" icon={<DatabaseOutlined ></DatabaseOutlined>}></ItemInfo>
-      <ItemInfo count={appCount} type="app" link="/app" icon={<AppstoreOutlined ></AppstoreOutlined>}></ItemInfo>
-      <ItemInfo count={configCount} type="config" link="/app" icon={<TableOutlined ></TableOutlined>}></ItemInfo>
-      <ItemInfo count={clientCount} type="client" link="/client" icon={<ShrinkOutlined ></ShrinkOutlined>}></ItemInfo>
-    </div>
-  );
-}
-const home: React.FC = () => {
-  return (
     <PageContainer pageHeaderRender={false}>
-      <Summary></Summary>
+      <Summary clientCount={serverNodeStatus.clientCount}></Summary>
+      {
+        <NodeStatiCharts  nodeStatiInfo= { serverNodeStatus.nodeStatiInfo }></NodeStatiCharts>
+      }
     </PageContainer>
   );
 }
