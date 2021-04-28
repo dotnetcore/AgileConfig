eccaa00fa05c71daad1d1af7a0a327a46fd7f343
kklldog <kklldog@qq.com>
2021-04-29 00:51:25 +0800

    完成字典转换JSON帮助类

---
+++ AgileConfig.Server.Common/DictionaryConvertToJson.cs
@@ -0,0 +1,91 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgileConfig.Server.Common
+{
+    public class DictionaryConvertToJson
+    {
+        public static string ToJson(IDictionary<string, string> dict)
+        {
+            Dictionary<string, object> root = new Dictionary<string, object>();
+            foreach (var kv in dict)
+            {
+                Generate(kv.Key, kv.Value, root);
+            }
+
+            return DictToJsonString(root);
+        }
+
+        private static string DictToJsonString(Dictionary<string, object> dict)
+        {
+            //var str = new StringBuilder();
+            //str.AppendLine("{");
+            //foreach (var kv in dict)
+            //{
+            //    var value = "";
+            //    if (kv.Value is string)
+            //    {
+            //        value = (string)kv.Value;
+            //        var kvStr = "  \"" + kv.Key + "\":" + "\"" + value + "\",";
+            //        str.AppendLine(kvStr);
+            //    }
+            //    else
+            //    {
+            //        value = DictToJsonString(kv.Value as Dictionary<string, object>);
+            //        var kvStr = "  \"" + kv.Key + "\":" + value + ",";
+            //        str.AppendLine(kvStr);
+            //    }
+            //}
+            //var index = str.ToString().LastIndexOf(',');
+            //str.Remove(index,1);
+            //str.AppendLine("}");
+
+            //return str.ToString();
+
+            return JsonConvert.SerializeObject(dict, Formatting.Indented);
+        }
+
+        private static void Generate(string key, string value, Dictionary<string, object> parent)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            var groupArr = key.Split(':');
+            if (groupArr.Length > 1)
+            {
+                var sonKey = groupArr[0];
+                var newArr = new string[groupArr.Length - 1];
+                for (int i = 1; i < groupArr.Length; i++)
+                {
+                    newArr[i - 1] = groupArr[i];
+                }
+                var otherKeys = string.Join(':', newArr);
+                if (parent.ContainsKey(sonKey))
+                {
+                    //如果已经有子字典
+                    var son = parent[sonKey] as Dictionary<string, object>;
+                    if (son != null)
+                    {
+                        Generate(otherKeys, value, son);
+                    }
+                }
+                else
+                {
+                    var son = new Dictionary<string, object>();
+                    Generate(otherKeys, value, son);
+                    parent.Add(sonKey, son);
+                }
+
+            }
+            else
+            {
+                parent.Add(key, value);
+            }
+
+        }
+    }
+}
---
+++ AgileConfig.Server.CommonTests/DictionaryConvertToJsonTests.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AgileConfig.Server.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgileConfig.Server.Common.Tests
+{
+    [TestClass()]
+    public class DictionaryConvertToJsonTests
+    {
+        [TestMethod()]
+        public void ToJsonTest()
+        {
+            var dict = new Dictionary<string, string>() {
+                {"a","1" }
+            };
+            var json = DictionaryConvertToJson.ToJson(dict);
+            Assert.IsNotNull(json);
+            Console.WriteLine(json);
+
+            dict = new Dictionary<string, string>() {
+                {"a","1" },
+                {"b","2" }
+            };
+            json = DictionaryConvertToJson.ToJson(dict);
+            Assert.IsNotNull(json);
+            Console.WriteLine(json);
+
+            dict = new Dictionary<string, string>() {
+                {"a","1" },
+                {"b","2" },
+                {"c:d","3" }
+            };
+            json = DictionaryConvertToJson.ToJson(dict);
+            Assert.IsNotNull(json);
+            Console.WriteLine(json);
+
+            dict = new Dictionary<string, string>() {
+                {"a","1" },
+                {"b","2" },
+                {"c:d","3" },
+                {"e","4" }
+            };
+            json = DictionaryConvertToJson.ToJson(dict);
+            Assert.IsNotNull(json);
+            Console.WriteLine(json);
+
+            dict = new Dictionary<string, string>() {
+                {"a","1" },
+                {"b","2" },
+                {"c:d","3" },
+                {"e","4" },
+                {"f:g","5" }
+            };
+            json = DictionaryConvertToJson.ToJson(dict);
+            Assert.IsNotNull(json);
+            Console.WriteLine(json);
+
+            dict = new Dictionary<string, string>() {
+                {"a","1" },
+                {"b","2" },
+                {"c:d","3" },
+                {"e","4" },
+                {"f:g","5" },
+                {"h:i:j","6" }
+            };
+            json = DictionaryConvertToJson.ToJson(dict);
+            Assert.IsNotNull(json);
+            Console.WriteLine(json);
+
+            dict = new Dictionary<string, string>() {
+                {"a","1" },
+                {"b","2" },
+                {"c:d","3" },
+                {"e","4" },
+                {"f:g","5" },
+                {"h:i:j","6" },
+                {"k","7" },
+            };
+            json = DictionaryConvertToJson.ToJson(dict);
+            Assert.IsNotNull(json);
+            Console.WriteLine(json);
+
+            dict = new Dictionary<string, string>() {
+                {"a","1" },
+                {"b","2" },
+                {"c:d","3" },
+                {"e","4" },
+                {"f:g","5" },
+                {"h:i:j","6" },
+                {"k","7" },
+                {"c:d1","8" },
+            };
+            json = DictionaryConvertToJson.ToJson(dict);
+            Assert.IsNotNull(json);
+            Console.WriteLine(json);
+
+            dict = new Dictionary<string, string>() {
+                {"a","1" },
+                {"b","2" },
+                {"c:d","3" },
+                {"e","4" },
+                {"f:g","5" },
+                {"h:i:j","6" },
+                {"k","7" },
+                {"c:d1","8" },
+                {"c:d2","9" },
+            };
+            json = DictionaryConvertToJson.ToJson(dict);
+            Assert.IsNotNull(json);
+            Console.WriteLine(json);
+
+            dict = new Dictionary<string, string>() {
+                {"a","1" },
+                {"b","2" },
+                {"c:d","3" },
+                {"e","4" },
+                {"f:g","5" },
+                {"h:i:j","6" },
+                {"k","7" },
+                {"c:d1","8" },
+                {"c:d2","9" },
+                {"c:d2:e","10" },
+                {"l","11" },
+                {"n","12" },
+            };
+            json = DictionaryConvertToJson.ToJson(dict);
+            Assert.IsNotNull(json);
+            Console.WriteLine(json);
+        }
+    }
+}
