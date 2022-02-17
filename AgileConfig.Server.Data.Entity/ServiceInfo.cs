147cf8bd09987f33067b1dcedc9b63d373d18161
mjzhou <kklldog@qq.com>
2022-02-17 17:47:15 +0800

    add service alive field

--- AgileConfig.Server.Data.Entity/ServiceInfo.cs
+++ AgileConfig.Server.Data.Entity/ServiceInfo.cs
@@ -5,29 +5,36 @@ using System.Text;
 
 namespace AgileConfig.Server.Data.Entity
 {
-    public enum SysLogType
+    public enum ServiceAlive
     {
-        Normal = 0,
-        Warn = 1
+        Offline = 0,
+        Online = 1
     }
 
-    [Table(Name = "agc_sys_log")]
-    [OraclePrimaryKeyName("agc_sys_log_pk")]
-    public class SysLog
+    [Table(Name = "agc_service_info")]
+    [OraclePrimaryKeyName("agc_sys_servieinfo_pk")]
+    public class ServiceInfo
     {
         [Column(Name = "id", IsIdentity = true)]
         public int Id { get; set; }
 
-        [Column(Name = "app_id", StringLength = 36)]
-        public string AppId { get; set; }
+        [Column(Name = "service_id", StringLength = 100)]
+        public string ServiceId { get; set; }
 
-        [Column(Name = "log_type")]
-        public SysLogType LogType { get; set; }
+        [Column(Name = "service_name", StringLength = 100)]
+        public string ServiceName { get; set; }
 
-        [Column(Name = "log_time")]
-        public DateTime? LogTime { get; set; }
+        [Column(Name = "ip", StringLength = 100)]
+        public string Ip { get; set; }
 
-        [Column(Name = "log_text", StringLength = 2000)]
-        public string LogText { get; set; }
+        [Column(Name = "port")]
+        public int? Port { get; set; }
+
+        [Column(Name = "meta_data", StringLength = 2000)]
+        public string MetaData { get; set; }
+
+        [Column(Name = "alive")]
+        public ServiceAlive Alive { get; set; }
     }
+  
 }
--- AgileConfig.Server.Data.Entity/SysLog.cs
+++ AgileConfig.Server.Data.Entity/SysLog.cs
@@ -11,26 +11,23 @@ namespace AgileConfig.Server.Data.Entity
         Warn = 1
     }
 
-    [Table(Name = "agc_service_info")]
-    [OraclePrimaryKeyName("agc_sys_servieinfo_pk")]
-    public class ServiceInfo
+    [Table(Name = "agc_sys_log")]
+    [OraclePrimaryKeyName("agc_sys_log_pk")]
+    public class SysLog
     {
         [Column(Name = "id", IsIdentity = true)]
         public int Id { get; set; }
 
-        [Column(Name = "service_id", StringLength = 100)]
-        public string ServiceId { get; set; }
+        [Column(Name = "app_id", StringLength = 36)]
+        public string AppId { get; set; }
 
-        [Column(Name = "service_name", StringLength = 100)]
-        public string ServiceName { get; set; }
+        [Column(Name = "log_type")]
+        public SysLogType LogType { get; set; }
 
-        [Column(Name = "ip", StringLength = 100)]
-        public string Ip { get; set; }
+        [Column(Name = "log_time")]
+        public DateTime? LogTime { get; set; }
 
-        [Column(Name = "port")]
-        public int? Port { get; set; }
-
-        [Column(Name = "meta_data", StringLength = 2000)]
-        public string MetaData { get; set; }
+        [Column(Name = "log_text", StringLength = 2000)]
+        public string LogText { get; set; }
     }
 }
