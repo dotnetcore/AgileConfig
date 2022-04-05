69ca50b28379572e87c602eb879989a6b956e756
kklldog <kklldog@qq.com>
2022-04-05 17:20:09 +0800

    ServiceItem vm

---
+++ AgileConfig.Server.UI/react-ui-antd/src/pages/Services/data.d.ts
@@ -0,0 +1,11 @@
+export type ServiceItem = {
+    id: string,
+    serviceId: string,
+    serviceName: string,
+    ip: string,
+    port: number,
+    checkUrl: string,
+    alarmUrl: string,
+    heartBeatMode: string
+  };
+    
