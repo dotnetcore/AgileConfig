c6f6982a79a4e24d79e25aa807b5f6b443960108
彭迁 <pengqian089@hotmail.com>
2023-12-25 14:42:03 +0800

    移除 Mongodb Service，使用 Mongodb Repository

--- test/AgileConfig.Server.Mongodb.ServiceTest/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using Moq;
-global using NUnit.Framework;
-global using AgileConfig.Server.Data.Mongodb;
-global using AgileConfig.Server.Data.Entity;
-global using AgileConfig.Server.IService;
-global using AgileConfig.Server.Mongodb.Service;
