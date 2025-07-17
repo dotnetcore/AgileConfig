c7c2dc2b2e8b838e855214ad7711c1c7f6d9f49b
agile.zhou <kklldog@live.cn>
2025-07-18 00:11:05 +0800

    Update version to 1.9.13

--- src/AgileConfig.Server.Data.Repository.Freesql/FreesqlRepositoryServiceRegister.cs
+++ src/AgileConfig.Server.Data.Repository.Freesql/FreesqlRepositoryServiceRegister.cs
@@ -22,30 +22,26 @@ namespace AgileConfig.Server.Data.Repository.Freesql
 
         public T GetServiceByEnv<T>(IServiceProvider sp, string env) where T : class
         {
+            var factory = sp.GetService<IFreeSqlFactory>();
+
             if (typeof(T) == typeof(IUow))
             {
-                var factory = sp.GetService<IFreeSqlFactory>();
-                var fsq = factory.Create(env);
-                return (new FreeSqlUow(fsq)) as T;
+                return (new FreeSqlUow(factory.Create(env))) as T;
             }
             if (typeof(T) == typeof(IConfigPublishedRepository))
             {
-                var factory = sp.GetService<IFreeSqlFactory>();
                 return new ConfigPublishedRepository(factory.Create(env)) as T;
             }
             if (typeof(T) == typeof(IConfigRepository))
             {
-                var factory = sp.GetService<IFreeSqlFactory>();
                 return new ConfigRepository(factory.Create(env)) as T;
             }
             if (typeof(T) == typeof(IPublishDetailRepository))
             {
-                var factory = sp.GetService<IFreeSqlFactory>();
                 return new PublishDetailRepository(factory.Create(env)) as T;
             }
             if (typeof(T) == typeof(IPublishTimelineRepository))
             {
-                var factory = sp.GetService<IFreeSqlFactory>();
                 return new PublishTimelineRepository(factory.Create(env)) as T;
             }
 
@@ -56,7 +52,7 @@ namespace AgileConfig.Server.Data.Repository.Freesql
         {
             var freesqlType = MyFreeSQL.ProviderToFreesqlDbType(provider);
 
-            return freesqlType.HasValue;
+            return freesqlType.HasValue ;
         }
     }
 }
